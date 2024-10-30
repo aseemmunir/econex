9935f534cbf9bbe6a4575218b257fc31c634dc83
Farhan Faryad <ranafarhan2808@gmail.com>
2024-10-30 13:00:16 +0500

    updated navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import '../../index.css'
+const Navbar= () => {
+  
+  const handleCall = () => {
+    window.location.href = 'tel:+1234567890';
+  };
 
-const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="w-full   py-3 px-5 flex justify-between items-center">
+      
+      <button 
+        onClick={handleCall} 
+        className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+      >
+        Call
+      </button>
+    </div>
+  );
+};
 
 export default Navbar;
