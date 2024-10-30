557d6381171e4c08b01113a8a5778a6f4b2cc623
tayyabihsan <tayyab.ihsan.dm@gmail.com>
2024-10-30 12:17:45 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,tsx, jsx}"],
   theme: {
     extend: {
       colors: {
-        'primary': '#121212',
-        'secondary': '#656565',
-        'grey': '#F63227',
-        'light' : '#E8E8E8',
-        'success' : '#27AE60',
-        'info' : '#2F80ED',
-        'warning' : '#E2B93B',
-        'error' : '#EB5757'
+        primary: '#121212',
+        secondary: '#656565',
+        grey: '#F63227',
+        light: '#E8E8E8',
+        success: '#27AE60',
+        info: '#2F80ED',
+        warning: '#E2B93B',
+        error: '#EB5757',
       },
       fontSize: {
-        'xl' : '46px',
-        'lg' : '34px',
-        'base' : '24px',
-        'sm' : '16px',
-        'xs' : '14px'
+        xl: '46px',
+        lg: '34px',
+        base: '24px',
+        sm: '16px',
+        xs: '14px',
       },
-
       fontFamily: {
-        'poppins' : 'Poppins, sans-serif'
-      }
+        poppins: ['Poppins', 'sans-serif'],  // Improved syntax
+      },
     },
   },
   plugins: [],
