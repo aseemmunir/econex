38eaedc78a75fa2e33dceedece0e1c41c41faec0
tayyabihsan <tayyab.ihsan.dm@gmail.com>
2024-10-31 18:22:52 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
-import Navbar from '../Components/Navbar/Navbar'
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
